996197ae004b5bf25fd8069e0dc7b7d4d58e0cf5
Johannes Rumpf <johannes.rumpf@gmail.com>
2022-03-24 22:06:44 +0100

    update / enhanced by master and dev branch

--- build.bat
+++ build.bat
@@ -1,3 +1,11 @@
-docker build -t chordpro:latest .
-docker tag chordpro:0.1 jayar79/chordpro:latest
+docker build -t chordpro:latest ./latest
+docker tag chordpro:latest jayar79/chordpro:latest
 docker push jayar79/chordpro:latest
+
+docker build -t chordpro:dev ./dev
+docker tag chordpro:dev jayar79/chordpro:dev
+docker push jayar79/chordpro:dev
+
+docker build -t chordpro:master ./master
+docker tag chordpro:master jayar79/chordpro:
+docker push jayar79/chordpro:master
