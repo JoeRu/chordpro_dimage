605aca071145ac4919ef63fb8dd3d8db81538d92
Johannes Rumpf <johannes.rumpf@gmail.com>
2024-10-21 22:45:01 +0200

    kleinere Fehler

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 docker build -t chordpro:dev_container ./dev_container
 docker tag chordpro:latest jayar79/chordpro:latest
-docker push jayar79/chordpro:latest
+docker push jayar79/chordpro:dev_container
 
 docker build --no-cache -t chordpro:dev ./dev
 docker tag chordpro:dev jayar79/chordpro:dev
