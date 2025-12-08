6314dc4e38283a1b3e9b532ddae0ea7c80512fc0
Johannes Rumpf <johannes.rumpf@gmail.com>
2025-12-08 18:53:43 +0100

    refactor

--- build_dev.bat
+++ build_dev.bat
@@ -2,10 +2,10 @@
 # docker tag chordpro:dev_container jayar79/chordpro:dev_container
 # docker push jayar79/chordpro:dev_container
 
-docker buildx build  --push --platform linux/amd64 --no-cache ./dev_container --target final -t jayar79/chordpro:dev_container
+docker buildx build  --push --platform linux/amd64 --no-cache ./dev_container -t jayar79/chordpro:dev_container
 
 # docker build --no-cache -t chordpro:dev ./dev
 # docker tag chordpro:dev jayar79/chordpro:dev
 # docker push jayar79/chordpro:dev
 
-docker buildx build  --push --platform linux/amd64 --no-cache ./dev --target final -t jayar79/chordpro:dev
+docker buildx build  --push --platform linux/amd64 --no-cache ./dev -t jayar79/chordpro:dev
